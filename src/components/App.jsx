d413baf30251741d961409b830442f2c8b29183d
Jenia Berestian <jberestian@gmail.com>
2022-08-26 09:40:42 +0300

    added changes in app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,7 @@ import '../reset.css';
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+   <>
+   </>
   );
 };
